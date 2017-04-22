61fa62a47d74d8d5a511bd67cb114a51620624c0
Wadum <chrwadum@gmail.com>
2017-04-22 10:45:33 +0200

    First networked RPC.

---
+++ Assets/Scripts/ToggleButtonColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ToggleButtonColor : Photon.MonoBehaviour {
+
+	Button button;
+
+	public void Start() {
+		this.button = GetComponentInParent<Button>();
+	}
+
+	public void ToggleColor() {
+		photonView.RPC("ToggleColorRPC", PhotonTargets.All);
+	}
+
+	[PunRPC]
+	public void ToggleColorRPC() {
+		ColorBlock colors = button.colors;
+
+		colors.highlightedColor = colors.normalColor = new Color((colors.normalColor.r + 1)%2, (colors.normalColor.g + 1)%2, colors.normalColor.b);
+
+		button.colors = colors;
+	}
+}
